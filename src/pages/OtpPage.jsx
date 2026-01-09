d2d74513af7920fb1732d001714d5105016d3ba6
seunfunmi <seunfunmisore89@gmail.com>
2026-01-09 19:15:18 +0100

    Changed a path

--- src/pages/OtpPage.jsx
+++ src/pages/OtpPage.jsx
@@ -1,4 +1,4 @@
-import { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator } from '@/components/ui/input-otp';
+import { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator } from '../components/ui/input-otp';
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
